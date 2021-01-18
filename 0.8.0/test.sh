d8bf8b6fbf05a103761d0043d192381dd8e56958
Ivan Cao-Berg <icaoberg@psc.edu>
2021-01-17 21:41:58 -0500

    Added simple test

---
+++ 0.8.0/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load xxhash
+
+wget --quiet --output-document=image.jpg https://pixabay.com/get/53e9d6404b51b108feda8460da29317f1536dde2575972_1920.jpg
+xxhsum image.jpg
+xxh32sum image.jpg
+xxh64sum image.jpg
+xxh128sum image.jpg
+rm -f image.jpg
